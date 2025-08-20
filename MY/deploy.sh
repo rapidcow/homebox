17783ab9dc6db628a9a71eb4adce2b7786e138f5
Charlie Root <root@mhfamily.yizx.net>
2025-08-20 23:36:11 +0000

    never mind, don't use stow
    stow can't do that (stowing an STOW/blah directory inside STOW/)
    https://github.com/aspiers/shell-env/issues/1#issuecomment-56317040

--- MY/deploy.sh
+++ MY/deploy.sh
@@ -6,5 +6,5 @@ git diff-index --exit-code HEAD -- >/dev/null || version="$version-dirty"
 (
     set -x
     make install prefix="/usr/local/opt/homebox@$version/homebox"
-    stow -d /usr/local/opt/"homebox@$version" homebox
+    ln -sf "homebox@$version" /usr/local/opt/homebox
 )
