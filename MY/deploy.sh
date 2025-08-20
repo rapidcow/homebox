31c3e5e981044a9bf35e1f831f2bfad289fe52a8
Charlie Root <root@mhfamily.yizx.net>
2025-08-20 23:15:12 +0000

    oh i can't stow opt subdirs into /opt....
    but i can probably still stow /opt/homebox@/* VERSION */
    into just /opt/homebox :D  i'll just... lose the suffices

--- MY/deploy.sh
+++ MY/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 prefix="/usr/local/opt/homebox@$(git describe --tags HEAD)"
+git diff-index --exit-code HEAD -- >/dev/null || prefix="$prefix-dirty"
 set -x
 make install prefix="$prefix"
