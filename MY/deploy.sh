6930dccdfa0a2accec0d8548282f6f28dbc8c0aa
Charlie Root <root@mhfamily.yizx.net>
2025-08-21 20:31:14 +0000

    deploy.sh was not re-linking correctly
    it created a link INSIDE /usr/local/opt/homebox.... XP

--- MY/deploy.sh
+++ MY/deploy.sh
@@ -6,5 +6,8 @@ git diff-index --exit-code HEAD -- >/dev/null || version="$version-dirty"
 (
     set -x
     make install prefix="/usr/local/opt/homebox@$version"
+    # ln -f seeems to still create the symlink inside
+    # the (symlinked) directory.
+    ! test -L /usr/local/opt/homebox || rm /usr/local/opt/homebox
     ln -sf "homebox@$version" /usr/local/opt/homebox
 )
